bed1306ad0fbbe655fc10be974230dbd7e83da31
邱毓 <xiaoqiuuu@gmail.com>
2024-12-26 17:02:49 +0800

    fixed:ipcRenderer Bug

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -9,6 +9,8 @@ import App from './App.vue';
 import router from './router';
 import './sidebar.js';
 
+// const { createDataTable } = require("./db/sqlite3db.js");
+// createDataTable();
 /*
 import './eventBus.js'
 
